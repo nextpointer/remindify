bc8c5b0c79e4c8a6bb3b0d376235c58db78f646a
lazy34 <maitysurajit0901@gmail.com>
2024-08-08 16:34:24 +0530

    Frontend : Some fixing

--- Frontend/lib/cursor.js
+++
@@ -1,6 +0,0 @@
-var cursor=document.querySelector('.cursor')
-var cursor2=document.querySelector('.cursor2')
-
-document.addEventListener('mousemove',(e)=>{
-    cursor.style.cssText=cursor2.style.cssText="left:"+e.clientX +"px; top:"+e.clientY+"px;"
-})
