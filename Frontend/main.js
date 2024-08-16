d0accb778e8ba3a0d96c4db01e02b08a395c8d7d
lazy34 <maitysurajit0901@gmail.com>
2024-08-16 15:08:16 +0530

    changes

--- Frontend/main.js
+++ Frontend/main.js
@@ -9,9 +9,6 @@ import {
   getRemindersFromDB,
 } from "./util/indexDB.js";
 
-import './font/GeistMonoVF.ttf'
-import './assets/delete.png'
-
 // SVG for redirect icon
 const redirectSVG = `<?xml version="1.0" encoding="utf-8"?>
 <svg width="800px" height="800px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#000000">
